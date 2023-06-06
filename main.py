d2096b3d0d630dfd5a5b39dde9681cd3bd58f3f7
vz-chen <118237300+vz-chen@users.noreply.github.com>
2023-06-05 22:55:36 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn #may need smth more specific
+import ImageSegmentation
+
+HAAR_CASCADE = "haarcascade_frontalface_default.xml"
