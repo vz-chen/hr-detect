4d497f3809bb6fb653ee06a0c942d3e3951ebdfd
evelina <91142355+135ze@users.noreply.github.com>
2023-06-16 02:08:05 -0400

    update main.py & revert ImageSegmentation.py

--- ImageSegmentation.py
+++ ImageSegmentation.py
@@ -1,41 +1,14 @@
 import sys
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn #may need smth more specific
-import ImageSegmentation
-from Grabcut import grabCut 
+from sklearn import mixture 
+import maxflow
 
-
-# ROI
-ITERATIONS = 0
-
-# GLOBAL FUNCTION
-ROIavgRBG = [] # stores the avg RBG values in each ROI (where analysis is performed)
-heartRates = [] # stores hr calculates 
-prevFaceBox = None # stores face box coordinates from previous frame
-
-# prepare camera capture
-cap = cv2.VideoCapture(1) # captures video from front camera
-HAAR_CASCADE = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(HAAR_CASCADE) # face detection trained to detect frontal faces
-
-# capture every frame
-while cap.isOpened():
-    ret, frame = cap.read() # ret: bool, is camera available; frame: gets next frame
-    if not ret:
-        break
-
-# OTHER FUNCTIONS
-def imageSegment(image):
-    # initial array to store all pixels of image to be labelled fg or bg later
-    mask = np.zeros(image.shape[:2], np.uint8) 
-    # array of zeros with size as image (height, width, ignoring the third tuple: channel)
-    # each element is a 8-bit unsigned binary integer
-
-    bgModel = np.zeros((1,65),np.float64) # use to estimate fg and bg models based on iterative gueseses
-    fgModel = np.zeros((1,65),np.float64) 
-
-    cv2.grabCut(image,mask,faceBox,bgModel,fgModel, ITERATIONS, cv2.GC_INIT_WITH_RECT)
-
-    return 0
+# calculate the number of pixels, flatten 3d array of pixels into a 2d array
+def getPixels(pixels3D):
+    numPixels = pixels3D.shape[0] * pixels3D.shape[1] # total number of pixels
+    pixels = np.empty((numPixels, 3)) # initialize empty array with row size numPixels (total pixels) and 3 cols
+    for i in range(3):
+        pixels[:,i] = pixels3D[:,:,i].flatten() # : means select all, pixels[:,i] -> select all rows from a col, pixels3D[:,:,i] -> select all row/cols from channel
+    return pixels
--- main.py
+++ main.py
@@ -4,5 +4,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn #may need smth more specific
 import ImageSegmentation
+from Grabcut import grabCut 
 
+
+# ROI
+ITERATIONS = 0
+
+# GLOBAL FUNCTION
+ROIavgRBG = [] # stores the avg RBG values in each ROI (where analysis is performed)
+heartRates = [] # stores hr calculates 
+prevFaceBox = None # stores face box coordinates from previous frame
+
+# prepare camera capture
+cap = cv2.VideoCapture(1) # captures video from front camera
 HAAR_CASCADE = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(HAAR_CASCADE) # face detection trained to detect frontal faces
+
+# capture every frame
+while cap.isOpened():
+    ret, frame = cap.read() # ret: bool, is camera available; frame: gets next frame
+    if not ret:
+        break
+
+# OTHER FUNCTIONS
+def imageSegment(image):
+    # initial array to store all pixels of image to be labelled fg or bg later
+    mask = np.zeros(image.shape[:2], np.uint8) 
+    # array of zeros with size as image (height, width, ignoring the third tuple: channel)
+    # each element is a 8-bit unsigned binary integer
+
+    bgModel = np.zeros((1,65),np.float64) # use to estimate fg and bg models based on iterative gueseses
+    fgModel = np.zeros((1,65),np.float64) 
+
+    cv2.grabCut(image,mask,faceBox,bgModel,fgModel, ITERATIONS, cv2.GC_INIT_WITH_RECT)
+
+    return 0
