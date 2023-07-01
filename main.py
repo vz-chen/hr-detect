8242ce92d3391292e996e29c6633eee591dfde9c
evelina <91142355+135ze@users.noreply.github.com>
2023-07-01 12:12:09 -0400

    finish all ROI functions - update getROI: include conditionals with REMOVE_EYES and FOREHEAD_ONLY - create distanceROI and getBestROI

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ import matplotlib.pyplot as plt
 import sklearn #may need smth more specific
 import ImageSegmentation
 # import grabcut 
-
+import random
 
 # ROI
+REMOVE_EYES = False
+FOREHEAD_ONLY = False
+ADD_BOX_ERROR = False
+
 NUM_ITERATIONS = 0
 faceBox = 0 #temp
+
+MIN_FACE_SIZE = 100
+
 WIDTH_FRAC = 0
 HEIGHT_FRAC = 0
+
+EYE_LOWER_FRAC = 0.25
+EYE_UPPER_FRAC = 0.5
+
+BOX_ERROR_MAX = 0.5
 
 # GLOBAL FUNCTION
 ROIavgRBG = [] # stores the avg RBG values in each ROI (where analysis is performed)
@@ -67,8 +79,67 @@ def getROI(image, faceBox):
 
     (x, y, w, h) = faceBox
 
-    # TO-DO: add options for removing eyes or foreheado only
+    if REMOVE_EYES:
+        bgMask[y + h * EYE_LOWER_FRAC : y + h * EYE_UPPER_FRAC, :] = True
+    if FOREHEAD_ONLY:
+        bgMask[y + h * EYE_LOWER_FRAC :, :] = True
 
     roi = np.ma.array(image, mask=bgMask) # Masked array
     return roi
 
+def distanceROI(roi1, roi2):
+    squareDistance = 0 # no need to square root for real distance because only used for comparison
+    for i in range(len(roi1)): # len is 2
+        squareDistance += (roi1[i] - roi2[i])**2 # distance between x and y coords of two points
+    return squareDistance
+
+def getBestROI(frame, faceCascade, previousFaceBox):
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # choose shade of grey
+    faces = faceCascade.detectMultiScale(grey, scaleFactor=1.1, 
+        minNeighbors=5, minSize=(MIN_FACE_SIZE, MIN_FACE_SIZE), flags=cv2.cv.CV_HAAR_SCALE_IMAGE) # detect faces
+    roi = None
+    faceBox = None
+
+    # If no face detected, use ROI from previous frame
+    if len(faces) == 0:
+        faceBox = previousFaceBox
+
+    # If many faces detected, use one closest to that from previous frame
+    elif len(faces) > 1:
+        if previousFaceBox is not None:
+            # Find closest to previous
+            minDist = distanceROI(previousFaceBox, face) # compare rest against first distance
+            for face in faces:
+                if distanceROI(previousFaceBox, face) < minDist:
+                    faceBox = face
+        else:
+            # Chooses largest box by area (most likely to be true face)
+            maxArea = 0
+            for face in faces:
+                if (face[2] * face[3]) > maxArea:
+                    faceBox = face
+
+    # If one face dectected, use face
+    else:
+        faceBox = faces[0]
+
+    if faceBox is not None:
+        if ADD_BOX_ERROR:
+            # add margin of error around faceBox coordinates
+            noise = []
+            for i in range(4):
+                noise.append(random.uniform(-BOX_ERROR_MAX, BOX_ERROR_MAX))
+            (x, y, w, h) = faceBox
+            x1 = x + int(noise[0] * w)
+            y1 = y + int(noise[1] * h)
+            x2 = x + w + int(noise[2] * w)
+            y2 = y + h + int(noise[3] * h)
+            faceBox = (x1, y1, x2-x1, y2-y1)
+
+        # Show rectangle
+        #(x, y, w, h) = faceBox
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
+
+        roi = getROI(frame, faceBox)
+
+    return faceBox, roi
